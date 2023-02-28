4cb7c6ff1e7d38cf83ec5e7cf370506219c2836b
Anastasia Tsygankova <tsygnas@gmail.com>
2023-02-28 19:01:00 +0300

    fix

--- app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
@@ -3,23 +3,19 @@ package com.netology.tabbedapplication;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,44 +29,24 @@ public class MainActivityTest {
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityTest() {
-        ViewInteraction tabView = onView(
+    public void mainActivityTest2() {
+
+        ViewInteraction tabs = onView(
+                allOf(withId(R.id.tabs)));
+        tabs.check(matches(isDisplayed()));
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.section_label),
+                        isDisplayed()));
+        textView.check(matches(withText("Page: 1")));
+
+        ViewInteraction linearLayout = onView(
                 allOf(withContentDescription("Tab 2"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.tabs),
-                                        0),
-                                1),
-                        isDisplayed()));
-        tabView.perform(click());
+                        withParent(withParent(withId(R.id.tabs)))));
+        linearLayout.check(matches(isDisplayed()));
+        linearLayout.perform(click());
 
-        ViewInteraction tabView2 = onView(
-                allOf(withContentDescription("Tab 1"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.tabs),
-                                        0),
-                                0),
-                        isDisplayed()));
-        tabView2.perform(click());
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+        textView.check(matches(isDisplayed()));
+        textView.check(matches(withText("Page: 2")));
     }
 }
